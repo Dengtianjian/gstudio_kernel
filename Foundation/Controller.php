29cbe00ffc36e54745e3406a744cef00a6d07dfd
Tianjian <mail@isdtj.com>
2021-02-21 22:28:51 +0800

    增加前端全局语言常量数组

--- Foundation/Controller.php
+++ Foundation/Controller.php
@@ -10,4 +10,15 @@ class Controller
   {
     return $this->$name;
   }
+  public function __construct()
+  {
+    $langJson = \json_encode($GLOBALS['GLANG']);
+    $multipleEncodeJSScript = <<<EOT
+      <script>
+      const GLANG=JSON.parse('$langJson');
+      console.log(GLANG);
+      </script>
+EOT;
+    print_r($multipleEncodeJSScript);
+  }
 }
