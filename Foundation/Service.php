440bc0228aca088293beb075574ead5d1b7ae492
Tianjian <mail@isdtj.com>
2021-03-20 12:29:19 +0800

    增加Service基类

---
+++ Foundation/Service.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace gstudio_kernel\Foundation;
+
+class Service
+{
+  protected static $tableName = "";
+  private static $ModelInstance = null;
+  protected static function Model()
+  {
+    $callClass = \get_called_class();
+    if (!$callClass::$tableName) {
+      Response::error(500, 500001, "Service的TableName未填写");
+    }
+    self::$tableName = $callClass::$tableName;
+    if (self::$ModelInstance === null) {
+      self::$ModelInstance = new Model(self::$tableName);
+    }
+
+    return self::$ModelInstance;
+  }
+}
