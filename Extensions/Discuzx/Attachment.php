70adcce100e7d37e90cc62e9a928a4d3047656d9
Tianjian <mail@isdtj.com>
2021-03-24 21:19:08 +0800

    增加Attachment类

--- Extensions/Discuzx/Attachment.php
+++ Extensions/Discuzx/Attachment.php
@@ -3,21 +3,30 @@
 namespace gstudio_kernel\Extensions\Discuzx;
 
 use gstudio_kernel\Foundation\Arr;
+use gstudio_kernel\Foundation\Model;
 
 class Attachment
 {
-  public static function upload($files, $saveDir = "common", $extid = 0, $forcename = "")
+  public static function upload($files, $tableName = "", $tid = 0, $pid = 0, $price = 0, $remote = 0, $saveDir = "common", $extid = 0, $forcename = "")
   {
     include_once \libfile("discuz/upload", "class");
     $upload = new \discuz_upload();
     $uploadResult = [];
-    $one = false;
+    $onlyOny = false;
     if (Arr::isAssoc($files)) {
-      $one = true;
+      $onlyOny = true;
       $files = [$files];
     } else {
       $files = array_values($files);
     }
+    $uid = \getglobal("uid");
+    $timestamp = \getglobal("timestamp");
+    $insertDatas = [];
+    $updateDatas = []; //TODO 更新 form_attachment 表的新增数据
+    if ($tableName) {
+      $insertDatas[$tableName] = [];
+    }
+    include_once \libfile("function/core");
     foreach ($files as $fileItem) {
       $upload->init($fileItem, $saveDir, $extid, $forcename);
       if ($upload->error()) {
@@ -30,6 +39,50 @@ class Attachment
         $upload->save(true);
         $saveFileName = explode("/", $upload->attach['attachment']);
         $path = $saveDir . "/" . $upload->attach['attachment'];
+        $aid = getattachnewaid($uid);
+        $width = 0;
+        $fileInfo = [];
+        if ($upload->attach['isimage']) {
+          $fileInfo['width'] = $upload->attach['imageinfo'][0];
+          $fileInfo['height'] = $upload->attach['imageinfo'][1];
+          $width = $fileInfo['width'];
+          if (!$width) {
+            $width = 0;
+          }
+        }
+        $insertData = array(
+          'aid' => $aid,
+          "tid" => $tid,
+          "pid" => $pid,
+          'uid' => $uid,
+          'dateline' => $timestamp,
+          'filename' => dhtmlspecialchars(censor($upload->attach['name'])),
+          'filesize' => $upload->attach['size'],
+          'attachment' => $upload->attach['attachment'],
+          'remote' => $remote,
+          "description" => "",
+          "readperm" => 0,
+          "price" => $price,
+          'isimage' => $upload->attach['isimage'],
+          'width' => $width,
+          'thumb' => 0,
+          "picid" => 0
+        );
+        if (!$tableName) {
+          $tableId = null;
+          if ($tid) {
+            $tableId = getattachtableid($tid);
+          } else {
+            $tableId = getattachtableid(time());
+          }
+          $tableName = "forum_attachment_" . $tableId;
+          if (!$insertDatas[$tableName]) {
+            $insertDatas[$tableName] = [];
+          }
+          array_push($insertDatas[$tableName], $insertData);
+        } else {
+          array_push($insertDatas[$tableName], $insertData);
+        }
         $fileInfo = [
           "path" => $path,
           "extension" => $upload->attach['extension'],
@@ -37,16 +90,22 @@ class Attachment
           "saveFileName" => $saveFileName[count($saveFileName) - 1],
           "size" => $upload->attach['size'],
           "type" => $upload->attach['type'],
-          "fullPath" => \getglobal("setting/attachurl") . $path
+          "fullPath" => \getglobal("setting/attachurl") . $path,
+          "aid" => $aid,
+          "tableId" => $tableId,
+          "tableName" => $tableName,
+          "aidencode" => \aidencode($aid, 0, $tid)
         ];
-        if ($upload->attach['isimage']) {
-          $fileInfo['width'] = $upload->attach['imageinfo'][0];
-          $fileInfo['height'] = $upload->attach['imageinfo'][1];
-        }
         $uploadResult[] = $fileInfo;
       }
     }
-    if ($one) {
+    foreach ($insertDatas as $tableName => $insertData) {
+      $attachmenModel = new Model($tableName);
+      $attachmenModel->batchInsertByMS($insertData)->save();
+    }
+    debug($insertDatas);
+
+    if ($onlyOny) {
       return $uploadResult[0];
     }
 
