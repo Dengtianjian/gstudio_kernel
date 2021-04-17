e4733881751a893a20e7fc0e388a3dd066c5ed89
Tianjian <mail@isdtj.com>
2021-04-17 22:09:10 +0800

    HTTP类增加临时get、post请求

--- Foundation/Http.php
+++ Foundation/Http.php
@@ -20,4 +20,42 @@ class Http
     }
     return $ip;
   }
+  public static function send()
+  {
+  }
+  public static function get()
+  {
+  }
+  public static function post()
+  {
+  }
+  public static function sGet($url, $post_data = [])
+  {
+    $postdata = http_build_query($post_data);
+    $options = array(
+      'http' => array(
+        'method' => 'GET',
+        'header' => 'Content-type:application/x-www-form-urlencoded',
+        'timeout' => 15 * 60 // 超时时间（单位:s）
+      )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    return $result;
+  }
+  public static function sPost($url, $post_data)
+  {
+    $postdata = http_build_query($post_data);
+    $options = array(
+      'http' => array(
+        'method' => 'POST',
+        'header' => 'Content-type:application/x-www-form-urlencoded',
+        'content' => $postdata,
+        'timeout' => 15 * 60 // 超时时间（单位:s）
+      )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    return $result;
+  }
 }
