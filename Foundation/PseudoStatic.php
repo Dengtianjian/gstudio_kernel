c745a254cc61397e04906f09661be0b6b6e4f897
Tianjian <mail@isdtj.com>
2021-05-03 12:19:29 +0800

    伪静态类完善

--- Foundation/PseudoStatic.php
+++ Foundation/PseudoStatic.php
@@ -8,7 +8,42 @@ namespace gstudio_kernel\Foundation;
  */
 class PseudoStatic
 {
-  static function replace($url, $params)
+  static $rules = [];
+  static function add($rules)
   {
+    self::$rules = \array_merge(self::$rules, $rules);
+  }
+  static function replace($alias, $params = [])
+  {
+    $alias = explode("/", $alias);
+    $value = self::$rules;
+    foreach ($alias as $aliasItem) {
+      $value = $value[$aliasItem];
+    }
+    return Str::replaceParams($value, $params);
+  }
+  static function match($alias, $params = [])
+  {
+    $result = self::replace($alias, $params);
+    $alias = explode("/", $alias);
+    $mergeData = [];
+    $previous = "";
+    $aliasCount = count($alias);
+    foreach ($alias as $index => $item) {
+      if ($previous) {
+        $mergeData[$previous][$item] = [];
+        if ($index == $aliasCount - 1) {
+          $mergeData[$previous][$item] = $result;
+        }
+      } else {
+        $mergeData[$item] = [];
+        if ($index == $aliasCount - 1) {
+          $mergeData[$item] = $result;
+        }
+      }
+      $previous = $item;
+    }
+
+    $GLOBALS['GURLS'] = Arr::merge($GLOBALS['GURLS'], $mergeData);
   }
 }
