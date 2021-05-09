9a6568f4943d8815f2483b6f32d2451bdcf4e395
Tianjian <mail@isdtj.com>
2021-05-10 07:52:00 +0800

    路由文件

---
+++ Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace gstudio_kernel;
+
+use gstudio_kernel\Foundation\Router;
+use gstudio_kernel\App\Api\GetGSetController;
+use gstudio_kernel\App\Main as Main;
+
+Router::get("_gset", GetGSetController::class);
+Router::view("_download", Main\DownloadAttachmentView::class);
+
+//* 扩展相关
+Router::view("_extensions", Main\Extensions\ExtensionListViewController::class);
+Router::post("_extension/install", Main\Extensions\InstallExtensionController::class);
