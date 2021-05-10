889799c9871d196a95374edc465420cf3c5cc846
Tianjian <mail@isdtj.com>
2021-05-10 21:26:23 +0800

    完善类

--- Foundation/Extension/ExtensionIuu.php
+++ Foundation/Extension/ExtensionIuu.php
@@ -3,6 +3,7 @@
 namespace gstudio_kernel\Foundation\Extension;
 
 use gstudio_kernel\Foundation\Config;
+use gstudio_kernel\Foundation\File;
 use gstudio_kernel\Foundation\Iuu;
 
 include_once \libfile("function/plugin");
@@ -40,9 +41,9 @@ class ExtensionIuu extends Iuu
     $sqlPath = $this->extensionsPath . "/Iuu/Install";
 
     if ($multipleEncode) {
-      $sqlPath .= "/" . \CHARSET . "/install.sql";
+      $sqlPath .= "/" . $this->Charset . "/install.sql";
       if (!\file_exists($sqlPath)) {
-        $sqlPath .= "/" . \CHARSET . ".sql";
+        $sqlPath .= "/" . $this->Charset . ".sql";
       }
     }
     if (!\file_exists($sqlPath)) {
@@ -56,5 +57,55 @@ class ExtensionIuu extends Iuu
 
     return $this;
   }
-  // TODO 升级相关
+  public function upgrade()
+  {
+    $upgradeFilesRootPath = $this->extensionsPath . "/Iuu/Upgrade/Files";
+    $namespace = $this->namespace;
+    $this->scanDirAndVersionCompare($upgradeFilesRootPath, function ($version, $fileName) use ($upgradeFilesRootPath, $namespace) {
+      $filePath = $upgradeFilesRootPath . "/$fileName.php";
+      if (\file_exists($filePath)) {
+        $className = $namespace . "\Iuu\Upgrade\Files\\$fileName";
+        $upgradeItemInstance = new $className();
+        $upgradeItemInstance->handle();
+      }
+    });
+    return $this;
+  }
+  public function runUpgradeSql()
+  {
+    $sqlFileDirPath = $this->extensionsPath . "/Iuu/Upgrade";
+    $multipleEncode = Config::get("multipleEncode", $this->pluginId);
+    if ($multipleEncode) {
+      $sqlFileDirPath .= "/" . $this->Charset;
+    } else {
+      $sqlFileDirPath .= "/SQL";
+    }
+    if (!is_dir($sqlFileDirPath)) {
+      return $this;
+    }
+
+    $this->scanDirAndVersionCompare($sqlFileDirPath, function ($version, $fileName) use ($sqlFileDirPath) {
+      $sqlFilePath = $sqlFileDirPath .= "/$fileName.sql";
+      if (!\file_exists($sqlFilePath)) {
+        return $this;
+      }
+      $sqlContent = \file_get_contents($sqlFilePath);
+      \runquery($sqlContent);
+    });
+    return $this;
+  }
+  public function clean()
+  {
+    $this->cleanInstall();
+    $this->cleanUpgrade();
+    return File::deleteDirectory($this->extensionsPath . "/Iuu");
+  }
+  public function cleanInstall()
+  {
+    return File::deleteDirectory($this->extensionsPath . "/Iuu/Install");
+  }
+  public function cleanUpgrade()
+  {
+    return File::deleteDirectory($this->extensionsPath . "/Iuu/Upgrade");
+  }
 }
