b67c972da323d63a2232a3408f661a5ec6bdf6e6
Tianjian <mail@isdtj.com>
2022-09-23 15:28:06 +0800

    production

--- Config.php
+++ Config.php
@@ -7,7 +7,8 @@ if (!defined("IN_DISCUZ")) {
 }
 
 $Config = [
-  "mode" => "development", //* production development
+  "mode" => "production", //* production development
   "multipleEncode" => true,
   "extensions" => true,
+  "version" => "0.2.50"
 ];
